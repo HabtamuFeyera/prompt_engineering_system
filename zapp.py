367e6abfb903ae175a1fb7c0c6867ba50f887fd3
HabtamuFeyera <habtamufeyera95@gmail.com>
2024-01-17 03:49:45 +0300

    add new files

--- zapp.py
+++ zapp.py
@@ -1,13 +1,10 @@
+import os
+from langchain.chat_models import ChatOpenAI
+from langchain_community.chat_models import ChatOpenAI
 
+os.environ["OPENAI_API_KEY"] = os.getenv("OPENAI_API_KEY") or "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"
 
-from config.config import Configuration
-
-# Initialize the configuration
-config_instance = Configuration()
-
-# Access configuration parameters
-api_key = config_instance.api_key
-max_attempts = config_instance.max_attempts
-log_level = config_instance.log_level
-
-
+chat = ChatOpenAI(
+    openai_api_key="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",
+    model='gpt-3.5-turbo'
+)
