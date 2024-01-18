e23692dcf5358f6005f2b481eb58d8ff52f26a40
HabtamuFeyera <habtamufeyera95@gmail.com>
2024-01-18 04:47:00 +0300

    modification

--- zapp.py
+++ zapp.py
@@ -2,9 +2,4 @@ import os
 from langchain.chat_models import ChatOpenAI
 from langchain_community.chat_models import ChatOpenAI
 
-os.environ["OPENAI_API_KEY"] = os.getenv("OPENAI_API_KEY") or "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"
 
-chat = ChatOpenAI(
-    openai_api_key="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",
-    model='gpt-3.5-turbo'
-)
